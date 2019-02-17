3800f3eda1e71cf26448591f1aeaaf1498939e39
Brian Vullo <vullo.b@husky.neu.edu>
2019-02-16 22:54:58 -0500

    added spaceship

--- assets/js/game.jsx
+++ assets/js/game.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import { Stage, Layer, Image } from 'react-konva';
+import { Stage, Layer, Image, Container } from 'react-konva';
 import useImage from 'use-image';
 import spaceShipImg from '../static/images/spaceship.svg';
 
@@ -9,9 +9,13 @@ export default function game_init(root, channel) {
   ReactDOM.render(<Game/>, root);
 }
 
-const SpaceshipImage = () => {
+const SpaceshipImage = (props) => {
   const [image] = useImage(spaceShipImg);
-  return <Image image={image} x={0} y={0}/>
+  return <Image {...{
+    ...props,
+    image: image,
+    height: 50,
+    width: 40}}/>
 }
 
 class Game extends React.Component {
@@ -20,18 +24,38 @@ class Game extends React.Component {
     super(props);
     const { channel } = this.props;
     this.state = {
-    
-
+      ship: {
+        x: 500,//window.innerWidth/2,
+        y: 500//(window.innerHeight - 60),
+      }
     }
     this.channel = channel
+    this.onKeyDown = this.onKeyDown.bind(this)
   }
 
+  onKeyDown(e) {
+    console.log(e.key)
+    const { ship } = this.state
+    const { key } = e;
+    if(key == "ArrowLeft" || key == "a") {
+        this.setState({ship: {...ship, x: ship.x - 5}});
+      } else if(e.key == "ArrowRight" || key == "d") {
+        this.setState({ship: {...ship, x: ship.x + 5}});
+      }
+    console.log(this.state.ship.x)
+    }
+
+
   render() {
-    return <Stage width={window.innerWidth} height={window.innerHeight}>
+    const { ship: { x, y}} = this.state;
+    console.log(x,y);
+    return <div tabIndex="0" onKeyDown={(e) => {console.log(e); this.onKeyDown(e)}}>
+      <Stage width={1000} height={700}>
       <Layer>
-        <SpaceshipImage/>
+          <SpaceshipImage {...{x,y}}/>
       </Layer>
     </Stage>
+  </div>
   }
 
 }
